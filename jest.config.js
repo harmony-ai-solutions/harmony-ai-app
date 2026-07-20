22e49ed8fa741d1d2d36612d8012ddbab3d75c91
RuntimeRacer <sge2022@gmail.com>
2026-07-20 23:15:32 +0200

    fix: lag in OC GUI due to large diff files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  preset: 'react-native',
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-fs|react-native-keychain|react-native-sqlite-storage|@react-native-documents|react-native-vector-icons|react-native-linear-gradient|react-native-safe-area-context|react-native-screens|react-native-paper)/)',
-  ],
+  preset: '@react-native/jest-preset',
   setupFiles: ['./jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-fs|react-native-keychain|react-native-sqlite-storage|@react-native-documents|react-native-vector-icons|react-native-linear-gradient|react-native-safe-area-context|react-native-screens|react-native-paper|react-native-config|uuid|react-native-track-player|@invertase|@react-native-google-signin|@react-native-community)/)',
+  ],
+  moduleNameMapper: {
+    '^@test-utils/database$': '<rootDir>/src/database/__test_utils__/testDatabase',
+  },
 };
