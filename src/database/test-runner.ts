f1d24c57024e312ccdca67fa686ea7dd4ccfc0d7
senjuLawliet <goutasalaa@gmail.com>
2026-07-22 20:20:57 +0100

    removed test files

--- src/database/test-runner.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Database Test Runner (for in-app testing)
- *
- * Re-exports the test runner from __tests__ for use in the app bundle.
- * This file exists outside __tests__ because Metro bundler excludes __tests__ directories by default.
- */
-
-import runAllTests from './__tests__/run-all-tests';
-export default runAllTests;
